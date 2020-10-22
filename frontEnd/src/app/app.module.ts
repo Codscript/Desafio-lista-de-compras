a4c352e138a19f2234820adc2e0f74581cb81a75
rafael3michelle <rafael3michelle@pop-os.localdomain>
2020-10-22 17:31:03 -0300

    components Crud criados

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -12,13 +12,21 @@ import { MatListModule } from "@angular/material/list";
 import { HeaderComponent } from './components/template/header/header.component';
 import { NavComponent } from './components/template/nav/nav.component';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { ProductCreatComponent } from './components/product/product-creat/product-creat.component';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    ProductCreatComponent,
+    ProductReadComponent,
+    ProductUpdateComponent,
+    ProductDeleteComponent
   ],
   imports: [
     BrowserModule,
