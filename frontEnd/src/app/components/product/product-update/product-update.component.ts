40910220e24f2c686fa2d3928661698514175d40
rafael3michelle <rafael3michelle@pop-os.localdomain>
2020-10-22 18:29:20 -0300

    Configurando update

--- frontEnd/src/app/components/product/product-update/product-update.component.ts
+++ frontEnd/src/app/components/product/product-update/product-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ProductService } from '../product.service';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-update',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductUpdateComponent implements OnInit {
 
-  constructor() { }
+  product: Product
 
+  constructor(private router: Router, private productService: ProductService, private route: ActivatedRoute) { }
+  // O sinal de `+` converte para number
   ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get("id")
+    this.productService.readById(id).subscribe(product => {
+    this.product = product
+  });
   }
 
+  updatePoduct(): void{
+    this.productService.update(this.product).subscribe(() =>{
+      this.productService.showOnMensage('Atulizado com sucesso!')
+      this.router.navigate(['/products'])
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
 }
